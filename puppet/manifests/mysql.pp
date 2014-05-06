e8fc83e659337bfd80503af7a4b95c64509530ec
Jesse Cascio <jesse.cascio@sycara.com>
2014-05-06 09:22:04 -0700

    mysql update

--- puppet/manifests/mysql.pp
+++ puppet/manifests/mysql.pp
@@ -1,30 +1,35 @@
-  exec { "yum-init":
-    command => "yum -y update; yum -y install wget nano",
-    path    => ['/usr/bin']
-  }
 
-  exec { "get-rpm":
-    command => "wget http://repo.mysql.com/mysql-community-release-el6-5.noarch.rpm -O ~/mysql-server.rpm",
-    path    => ['/usr/bin'],
-    require => Exec['yum-init']
+  # http://docs.puppetlabs.com/references/latest/type.html
+  # http://matthewturland.com/2012/02/13/setting-up-ec2-for-drupal-with-puppet/
+  # yum -y update
+
+  exec { "yum-init":
+    command => "yum -y install wget nano",
+    path    => ["/bin", "/usr/bin"],
+    timeout => 0
   }
 
   exec { "update-rpm":
-    command => "rpm -ivh ~/mysql-server.rpm",
-    path    => ['/bin'],
-    require => Exec['get-rpm']
+    command => "wget http://repo.mysql.com/mysql-community-release-el6-5.noarch.rpm -O ~/mysql-server.rpm; rpm -ivh ~/mysql-server.rpm",
+    path    => ["/bin", "/usr/bin"],
+    require => Exec["yum-init"]
   }
   
-  # move this to a Package
-  exec { "mysql-install":
-    command => "yum -y install mysql mysql-server",
-    path    => ['/usr/bin'],
-    require => Exec['update-rpm']
+  package { "mysql": 
+    ensure  => "present",
+    require => Exec["update-rpm"]
+  }
+
+  package { "mysql-server": 
+    ensure  => "present",
+    require => Exec["update-rpm"]
   }
 
   service {"mysqld":
     enable  => true,
     ensure  => running,
-    require => Exec['mysql-install']
+    require => [
+      Package["mysql"],
+      Package["mysql-server"]
+    ]
   }
-
