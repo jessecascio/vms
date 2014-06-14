8e2de25b3372d28c12f5fdccebfed55d91919c84
Jesse Cascio <jessecascio@gmail.com>
2014-06-14 13:50:44 -0700

    default centos setup installs nano

--- puppet/centos/modules/yum/manifests/update.pp
+++ puppet/centos/modules/yum/manifests/update.pp
@@ -2,7 +2,7 @@
 class yum::update {
 
   exec { "yum-init":
-    command => "yum -y update",
+    command => "yum -y update; yum -y install nano",
     path    => ["/bin", "/usr/bin"],
     timeout => 0
   }
