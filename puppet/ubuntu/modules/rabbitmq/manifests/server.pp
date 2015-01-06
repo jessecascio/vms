b89f883fae92e93bed2849bcc0aa732028d7a5f5
Jesse Cascio <jessecascio@gmail.com>
2015-01-06 13:35:55 -0700

    set default erlang cookie

--- puppet/ubuntu/modules/rabbitmq/manifests/server.pp
+++ puppet/ubuntu/modules/rabbitmq/manifests/server.pp
@@ -1,6 +1,6 @@
 
 class rabbitmq::server {
-
+	
 	exec { "apt-rabbitmq":
 		command => "sudo apt-get install erlang -y;
 					sudo apt-get install erlang-nox -y;
@@ -10,17 +10,17 @@ class rabbitmq::server {
 					sudo apt-get update;
 					sudo apt-get install rabbitmq-server -y;
 					sudo rabbitmq-plugins enable rabbitmq_management;
-					sudo service rabbitmq-server start;",
+					sudo service rabbitmq-server start;
+					sudo rabbitmq-plugins enable rabbitmq_management;
+					sudo rabbitmqctl add_user jesse pass; 
+					sudo rabbitmqctl set_user_tags jesse administrator; 
+					sudo rabbitmqctl set_permissions -p / jesse '.*' '.*' '.*';
+					sudo rm /var/lib/rabbitmq/.erlang.cookie
+					echo 'jesse54ac1be08ea64' | sudo tee -a /var/lib/rabbitmq/.erlang.cookie
+					sudo chmod 400 /var/lib/rabbitmq/.erlang.cookie
+					sudo chown rabbitmq:rabbitmq /var/lib/rabbitmq/.erlang.cookie 
+					sudo service rabbitmq-server restart;",
 		path    => ["/bin", "/usr/bin"],
 		timeout => 0
-	}
-
-	file { "rabbitmq.config":
-	    path    => "/etc/rabbitmq/rabbitmq.config",
-	    ensure  => file,
-	    require => [
-	      Package["rabbitmq-server"]
-	    ],
-	    content => template("rabbitmq/cluster.erb")
 	}
 }
